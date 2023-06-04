eebfcaa0a2a843d8d2bb4f6e4c6479446bca5608
ISHIKURA Kiwamu <ishikura.kiwamu@gmail.com>
2023-06-04 15:20:32 +0900

    add testthat

---
+++ R/diagnoser.R
@@ -0,0 +1,7 @@
+#' diagnoser: A package for diagnosis of statistical models
+#'
+#' The diagnoser package provides diagnosis set of statistical models. It also provides the plot of diagnosis.
+#'
+#' @docType package
+#' @name diagnoser
+NULL
--- R/plot_diagnosis.R
+++ R/plot_diagnosis.R
@@ -1,21 +1,27 @@
 #' Conversion from P-value to expression
 #' @param p P-value
+#' @return a vector of expression.
 #' @examples
 #' p <- c(1e-05, 0.002, 0.03, 0.4)
 #' p2lt(p)
+#' @export
 p2lt <- function(p)
-  ifelse(p < 0.001, as.character(expression(italic(P) < 0.001)),
-         ifelse(p < 0.01, as.character(expression(italic(P) < 0.01)),
-                ifelse(p < 0.05, as.character(expression(italic(P) < 0.05)),
-                       as.character(expression(italic(P) == round(p, 2))))))
+  ifelse(!is.finite(p), NA,
+         ifelse(p < 0.001, as.character(expression(italic(P) < 0.001)),
+                ifelse(p < 0.01, as.character(expression(italic(P) < 0.01)),
+                       ifelse(p < 0.05, as.character(expression(italic(P) < 0.05)),
+                              as.character(expression(italic(P) == round(p, 2)))))))
 
 
 #' Conversion from P-value to stars
 #' @param p P-value
+#' @return a vector of character
 #' @examples
 #' p <- c(1e-05, 0.002, 0.03, 0.4)
 #' p2star(p)
+#' @export
 p2star <- function(p)
-  ifelse(p < 0.001, "***",
-         ifelse(p < 0.01, "**",
-                ifelse(p < 0.05, "*", "ns")))
+  ifelse(!is.finite(p), NA,
+         ifelse(p < 0.001, "***",
+                ifelse(p < 0.01, "**",
+                       ifelse(p < 0.05, "*", "ns"))))
