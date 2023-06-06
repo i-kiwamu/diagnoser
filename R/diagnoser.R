b916228feb88bf73ee2300fb97cea3f652aa6b3e
ISHIKURA Kiwamu <ishikura.kiwamu@gmail.com>
2023-06-06 17:49:50 +0900

    change name package root R file from diagnoser.R to diagnoser-package.R

--- R/diagnoser.R
+++
@@ -1,7 +0,0 @@
-#' diagnoser: A package for diagnosis of statistical models
-#'
-#' The diagnoser package provides diagnosis set of statistical models. It also provides the plot of diagnosis.
-#'
-#' @docType package
-#' @name diagnoser
-NULL
