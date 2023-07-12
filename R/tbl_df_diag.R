5d5c76adb6f6268050b1aa0aad5eec3a52e04f97
ISHIKURA Kiwamu <ishikura.kiwamu@gmail.com>
2023-07-12 19:49:55 +0900

    add names() of tbl_df_diag

--- R/tbl_df_diag.R
+++ R/tbl_df_diag.R
@@ -1,6 +1,7 @@
 # S3 class for tbl_df_diag
 
-#' Create a object of tbl_df_diag, subclass of tibble
+#' new_tibble_diag
+#' @description Create a object of tbl_df_diag, subclass of tibble
 #' @importFrom tibble is_tibble
 #' @param x tibble object (level = 1)
 #' @param xg tibble object of higher levels
@@ -14,10 +15,28 @@ new_tibble_diag <- function(x, xg, model) {
 }
 
 
-#' Print of tbl_df_diag
+#' print.tbl_df_diag
+#' @description Print of tbl_df_diag
 #' @inheritParams tibble::print.tbl_df
 print.tbl_df_diag <- function(x, width = NULL, ..., n = NULL,
                               max_extra_cols = NULL,
                               max_footer_lines = NULL) {
   NextMethod()
+}
+
+
+#' names.tbl_df_diag
+#' @export
+names.tbl_df_diag <- function(x) {
+  xt <- x
+  class(xt) <- class(xt)[-1]
+  result <- list(colnames(xt))
+  model_class <- class(attr(x, "model"))
+  if (is.element(model_class, c("lme", "lmerMod"))) {
+    xs <- attr(x, "levels")
+    for (l in names(xs)) {
+      result[[l]] <- colnames(xs[[l]])
+    }
+  }
+  return(result)
 }
